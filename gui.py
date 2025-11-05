54a3c70d0d44ff21c64db70db84ae52ad98d8593
kbb306 <kylerbbelshaw@gmail.com>
2025-11-04 19:20:50 -0700

    Added window skeleton.

---
+++ gui.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+class mainWindow:
+    def __init__(self,root):
+        self.root = root
+        self.root.title("Calendar Parser Without a Cool Acronym")
+        self.root.geometry("400x300")
