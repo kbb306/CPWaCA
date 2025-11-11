b33e89095c44660cedb7caf78089a021671d4694
kbb306 <kylerbbelshaw@gmail.com>
2025-11-11 12:43:07 -0700

    Modifed scopes

--- authorize.py
+++ authorize.py
@@ -7,7 +7,8 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets",
+          "https://www.googleapis.com/auth/drive"]
 
 # The ID and range of a sample spreadsheet.
 
