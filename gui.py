1726682c557fa01fe3dab9e3acad59d127825d17
kbb306 <kylerbbelshaw@gmail.com>
2025-11-25 10:54:45 -0700

    Showed better error message on sync()

--- gui.py
+++ gui.py
@@ -43,8 +43,8 @@ class mainWindow:
         self.datecheck()
         try:
             self.reader.sync()
-        except:
-            print("Warning, no reader class yet.")
+        except Exception as e:
+            print(f"Warning sync failed due to {e}")
        
 
     def fileFuckery(self,command,file,section,lookfor,changeTo=None):
